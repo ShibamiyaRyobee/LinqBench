2aac6d1099aed4c8a5863b2d0c4b2d7c5a40ae11
ShibamiyaRyobee <140879540+ShibamiyaRyobee@users.noreply.github.com>
2023-11-19 10:56:38 +0900

    プロジェクト ファイルを追加します。

---
+++ LinqBench/Program.cs
@@ -0,0 +1,11 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace LinqBench;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        BenchmarkRunner.Run<WhereAndFirst>();
+    }
+}
---
+++ LinqBench/WhereAndFirst.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace LinqBench;
+
+/// <summary>
+/// 検索用データ保持クラス
+/// </summary>
+public class TestClass
+{
+    public int Prop1 { get; set; }
+
+    public int Prop2 { get; set; }
+}
+
+[MemoryDiagnoser]
+public class WhereAndFirst
+{
+    private List<TestClass>? _list;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _list = Enumerable.Range(0, 10000).Select(x => new TestClass { Prop1 = x }).ToList();
+    }
+
+    [Benchmark]
+    public TestClass ToListCultist()
+    {
+        var narrowedList = _list!.Where(x => x.Prop1 > 4000).ToList();
+        return narrowedList.First(x => x.Prop1 == 4345);
+    }
+
+    [Benchmark]
+    public TestClass Normal()
+    {
+        var narrowedEnumerable = _list!.Where(x => x.Prop1 > 4000);
+        return narrowedEnumerable.First(x => x.Prop1 == 4345);
+    }
+}
